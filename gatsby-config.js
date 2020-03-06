3887a73d1ab162c858cb3cff21b228c10ab4c576
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-03-06 21:37:22 +0900

    testCommit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'きばやしの雑多なブログ',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'About Me',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
