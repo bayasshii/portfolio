be0db3526b109e303c876314d1504eb831fdb45a
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-03-07 15:25:53 +0900

    いらんコンポーネントの削除

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  position: absolute !important;
+  top: 0;
+  right: 0;
+`
+const NavWrap = styled.nav`
+  margin-left: auto;
+  padding-right: 100px;
+`
+const NavList = styled.nav`
+  display: flex;
+  & a {
+    color: #fff;
+    padding: 30px 10px;
+  }
+`
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -34,66 +50,16 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#0099ff" fill-opacity="1" d="M0,288L40,288C80,288,160,288,240,250.7C320,213,400,139,480,90.7C560,43,640,21,720,64C800,107,880,213,960,218.7C1040,224,1120,128,1200,90.7C1280,53,1360,75,1400,85.3L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
-          </svg>
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav>
+        <NavWrap>
+          <NavList>
+            <Link to="/">HOME</Link>
+            <Link to="/about">ABOUT</Link>
+            <Link to="/blog">BLOG</Link>
+            <Link to="/contact">Contact</Link>
+          </NavList>
+        </NavWrap>
+      </Nav>
     )
   }
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,160 +6,16 @@ import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
-function Icon() {
-  return (
-    <svg version="1.1" id="レイヤー_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 338">
-      <g id="Page-1">
-        <g id="Desktop">
-          <g id="Header">
-            <linearGradient id="Rectangle_2_" gradientUnits="userSpaceOnUse" x1="433.5762" y1="296.5683" x2="434.5762" y2="296.5683" gradientTransform="matrix(1024 0 0 -337.5626 -443982 100279.1484)">
-              <stop offset="0" style={{stopColor:'#FD57A3'}}/>
-              <stop offset="0.4308" style={{stopColor:'#7571DB'}}/>
-              <stop offset="1" style={{stopColor:'#AEF779'}}/>
-            </linearGradient>
-            <path id="Rectangle" class="st1" d="M0,0h1024v308.062c-86.768,19.667-177.445,29.501-272.031,29.501
-              c-141.879,0-353.036-110.019-477.344-110.019c-82.872,0-174.414,16.787-274.626,50.361V0z"/>
-
-              <linearGradient id="Rectangle_3_" gradientUnits="userSpaceOnUse" x1="433.5762" y1="296.5766" x2="434.5762" y2="296.5766" gradientTransform="matrix(1024 0 0 -315 -443982 93579.1172)">
-              <stop offset="0" style={{stopColor:'#FD57A3'}}/>
-              <stop offset="0.4308" style={{stopColor:'#7571DB'}}/>
-              <stop offset="1" style={{stopColor:'#49415E'}}/>
-            </linearGradient>
-            <path id="Rectangle_1_" class="st2" d="M0,0h1024v260.486C939.45,296.829,843.117,315,735.003,315
-              c-162.172,0-291.238-68.986-459.361-68.986c-112.082,0-203.963,10.77-275.642,32.309V0z"/>
-          </g>
-        </g>
-      </g>
-    </svg>
-  );
-}
-
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
+export const IndexPageTemplate = () => (
   <div>
-    <div>
-      {Icon()}
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    ああ
   </div>
 )
 
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
-
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-
   return (
     <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
+      <IndexPageTemplate/>
     </Layout>
   )
 }
@@ -173,41 +29,3 @@ IndexPage.propTypes = {
 }
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-      }
-    }
-  }
-`
