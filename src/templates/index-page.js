4ba7163d68ec63a4337f5d26d978374912889608
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-03-11 15:50:58 +0900

    twittercard修正

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 import BlogRoll from '../components/BlogRoll'
 import Helmet from 'react-helmet'
 import WidthLayout from '../components/WidthLayout'
 
-export const IndexPageTemplate = ({
-  helmet
-}) => {
+export const IndexPageTemplate = () => {
   return(
     <React.Fragment>
         <WidthLayout>
@@ -17,25 +14,11 @@ export const IndexPageTemplate = ({
   )
 }
 
-IndexPageTemplate.propTypes = {
-  helmet: PropTypes.object
-}
-
 class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <IndexPageTemplate
-          helmet={
-            <Helmet>
-              <meta name="twitter:card" content="summary"/>
-              <meta property="og:url" content="https:kibamasa.com"/>
-              <meta property="og:title" content="きばやしのブログ"/>
-              <meta property="og:description" content="きばやしの日常の雑多を書きます"/>
-              <meta property="og:image" content="https://treeethreee.s3-ap-northeast-1.amazonaws.com/twitterCardHome.jpg"/>
-            </Helmet>
-          }
-        />
+        <IndexPageTemplate/>
       </Layout>
     )
   }
