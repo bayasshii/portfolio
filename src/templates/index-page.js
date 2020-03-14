0ca1c9140248ba99e68409f531e9f95a9276ac1e
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-03-15 02:04:08 +0900

    ミスタイプ

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -11,7 +11,7 @@ export const IndexPageTemplate = () => {
       <Helmet>
         <title>【きばやしの日常】</title>
         <meta name="twitter:card" content="summary"/>
-        <meta property="og:url" content="https:kibamasa.com"/>
+        <meta property="og:url" content="https://kibamasa.com"/>
         <meta property="og:title" content="【きばやしの日常】"/>
         <meta property="og:description" content="他愛ない日常の様子を綴ります"/>
         <meta property="og:image" content="https://treeethreee.s3-ap-northeast-1.amazonaws.com/twitterCardHome.jpg"/>
