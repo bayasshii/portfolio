0dc3306c8b9e58935d3607068946a495c0e664a6
bayasshii <42494214+bayasshii@users.noreply.github.com>
2020-04-12 10:42:23 +0900

    cssにしてみる

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -5,71 +5,22 @@ import PreviewCompatibleImage from './PreviewCompatibleImage'
 import styled from 'styled-components'
 
 const BrogRollWrap = styled.div`
-  padding-bottom: 50px;
 `
 const BrogRollItemWrap = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
 `
 const BrogRollItem = styled.article`
-  box-sizing: border-box;
-  width: 100%;
-  margin-bottom: 30px;
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    padding: 0!important;
-  }
 `
 const BrogRollItemLink = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
 `
 const BrogRollItemTextWrap = styled.div`
-  padding: 0 0 0 20px;
-  width: 450px;
-  position: relative;
-  box-sizing: border-box;
-  @media screen and (max-width: 768px) {
-    padding: 10px 8px 30px;
-  }
 `
 const BrogRollItemTextTitle = styled.div`
-  font-size: 16px;
-  font-weight: 600;
 `
 const BrogRollItemTextDate = styled.div`
-  font-size: 12px;
-  @media screen and (max-width: 768px) {
-    font-size: 10px;
-  }
 `
 const BrogRollItemTextIntro = styled.div`
-  color: #ADADAD;
-  font-size: 14px;
-  width: 100%;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-  padding: 20px 0 0 0;
-  line-height: 20px;
-  @media screen and (max-width: 768px) {
-    padding: 7px 0 0;
-    font-size: 12px;
-  }
 `
 const BrogRollItemTextTag = styled.div`
-  display: flex;
-  padding-left: 20px;
-  & div {
-    padding-right: 10px;
-    font-size: 12px;
-    @media screen and (max-width: 768px) {
-      font-size: 10px;
-    }
-  }
 `
 
 class BlogRoll extends React.Component {
@@ -78,13 +29,13 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <BrogRollWrap>
-        <BrogRollItemWrap>
+      <BrogRollWrap className="brogRollWrap">
+        <BrogRollItemWrap className="brogRollItemWrap">
           {posts &&
             posts.map(({ node: post }) => (
-              <BrogRollItem key={post.id}>
+              <BrogRollItem key={post.id} className="brogRollItem">
                 <Link to={post.fields.slug}>
-                  <BrogRollItemLink>
+                  <BrogRollItemLink className="brogRollItemLink">
                     {post.frontmatter.featuredimage ? (
                       <PreviewCompatibleImage
                         imageInfo={{
@@ -93,11 +44,11 @@ class BlogRoll extends React.Component {
                         }}
                       />
                     ) : null}
-                    <BrogRollItemTextWrap>
-                      <BrogRollItemTextTitle>{post.frontmatter.title}</BrogRollItemTextTitle>
+                    <BrogRollItemTextWrap className="brogRollItemTextWrap">
+                      <BrogRollItemTextTitle className="brogRollItemTextTitle">{post.frontmatter.title}</BrogRollItemTextTitle>
                       <div style={{display:'flex', padding:'5px 0 0 0'}}>
-                        <BrogRollItemTextDate>{post.frontmatter.date}</BrogRollItemTextDate>
-                        <BrogRollItemTextTag>
+                        <BrogRollItemTextDate className="brogRollItemTextDate">{post.frontmatter.date}</BrogRollItemTextDate>
+                        <BrogRollItemTextTag className="brogRollItemTextTag">
                           { post.frontmatter.tags.map((tag) => (
                             tag ? (
                               <div>{tag}</div>
@@ -105,10 +56,9 @@ class BlogRoll extends React.Component {
                           ))}
                         </BrogRollItemTextTag>
                       </div>
-                      <BrogRollItemTextIntro>{post.excerpt}</BrogRollItemTextIntro>
+                      <BrogRollItemTextIntro className="brogRollItemTextIntro">{post.excerpt}</BrogRollItemTextIntro>
                     </BrogRollItemTextWrap>
                   </BrogRollItemLink>
-
                 </Link>
               </BrogRollItem>
             ))}
